05654cf21302103058e3767368b691bc695d9562
jinhang <jinhang87@163.com>
2021-11-03 00:24:32 +0800

    window

--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,7 @@
-# Hello
-Vuepress Plugin Cnzz!
+## Usage
+
+1. Install this plugin :
+
+```sh
+yarn add @jinhang/vuepress-plugin-cnzz -D
+```
--- packages/src/client/composables/useCnzz.ts
+++ packages/src/client/composables/useCnzz.ts
@@ -1,9 +1,9 @@
 export const useCnzz = (id: string): void => {
   console.log('useCnzz');
 
-  const cnzzScript = document.createElement('script')
+  const cnzzScript = window?.document.createElement('script')
   cnzzScript.type = 'text/javascript'
   cnzzScript.src = `https://s9.cnzz.com/z_stat.php?id=${id}&web_id=${id}`
   cnzzScript.async = true
-  document.head.appendChild(cnzzScript)
+  window?.document.head.appendChild(cnzzScript)
 }
